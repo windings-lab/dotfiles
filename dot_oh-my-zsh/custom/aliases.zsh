33649bf512d4eeede5ccc5a3e5c7b360152506c3
Anton Zhukov <janton20001@gmail.com>
2025-11-21 01:39:42 +0200

    Update .oh-my-zsh/custom/aliases.zsh

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -7,6 +7,8 @@ alias ssh-agent-start='eval "$(ssh-agent -s)"'
 sshadd() {
   ssh-add "$HOME/.ssh/$1"
 }
-
+compdef '_files -W ~/.ssh' sshadd
 alias ssh-add="sshadd"
+compdef '_files -W ~/.ssh' ssh-add
+
 alias ssh-agent-kill="ssh-agent -k"
