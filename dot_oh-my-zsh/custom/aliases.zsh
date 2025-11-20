e7e386cb268afa616b9a3eb079a7dc7ab2ef3e74
Anton Zhukov <janton20001@gmail.com>
2025-11-21 01:26:44 +0200

    Update .oh-my-zsh/custom/aliases.zsh

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -3,3 +3,5 @@ alias p-ls="pacman -Q"
 alias p-lse="pacman -Qe"
 alias d-edit="chezmoi edit --watch"
 alias ssh-agent-start='eval "$(ssh-agent -s)"'
+alias ssh-add="ssh-add ~/.ssh/"
+alias ssh-agent-kill='ssh-agent -k"
