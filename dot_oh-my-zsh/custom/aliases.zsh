0cc8789f34b77b1cbaa3cdd1e7b9ed5fb5687fc3
Anton Zhukov <janton20001@gmail.com>
2025-11-21 14:43:37 +0200

    Update .oh-my-zsh/custom/aliases.zsh

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -1,6 +1,10 @@
-alias p-ins="pacman -S"
-alias p-ls="pacman -Q"
-alias p-lse="pacman -Qe"
+OS_ID=$(grep '^ID=' /etc/os-release | cut -d= -f2 | tr -d '"')
+
+if [[ "$OS_ID" = "arch" ]]; then
+    alias p-ins="pacman -S"
+    alias p-ls="pacman -Q"
+    alias p-lse="pacman -Qe"
+fi
 alias d-edit="chezmoi edit --watch"
 alias ssh-agent-start='eval "$(ssh-agent -s)"'
 
